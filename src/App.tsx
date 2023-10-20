c3684a59c40d5875ab8cd694d512f86bf86d33f7
tamashiro-syuta <syuta5825@gmail.com>
2023-10-20 15:30:02 +0900

    ✨ react-hook-formの公式のサンプルにコメントを追

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm, SubmitHandler } from "react-hook-form"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+type Inputs = {
+  example: string
+  exampleRequired: string
 }
 
-export default App
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+  // exampleという名前のinputの値を監視する
+  console.log(watch("example"))
+
+  return (
+    // "handleSubmit"は、"onSubmit"を呼び出す前に入力を検証します。
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register関数を呼び出して、入力をフックに登録する。 */}
+      <input defaultValue="test" {...register("example")} />
+
+      {/* 第2引数にはバリデーションを入れる */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* フィールドのバリデーションに失敗した場合、エラーが返されます。 */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  )
+}
